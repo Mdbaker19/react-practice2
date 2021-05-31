b460184209f2398ce5aa989764b73d18854ed1d6
Matthew <matthew.d.baker20@gmail.com>
2021-05-31 17:58:58 -0500

    added use effect to replace extra code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,20 @@
 import './LoginForm.css'
-import {useState} from "react";
+import { useState, useEffect } from "react";
 const LoginForm = props => {
 
     const [emailInput, setEmailInput] = useState("");
+    const [emailValid, setEmailValid] = useState(true);
+    const [passwordValid, setPasswordValid] = useState(true);
     const [passwordInput, setPasswordInput] = useState("");
 
-    const [loginInBtn, setLoginBtn] = useState(false);
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    useEffect(() => {
+        setFormIsValid(emailInput.includes("@") &&
+            emailInput.includes(".") &&
+            passwordInput.trim().length > 6)
+    }, [emailInput, passwordInput]);
+
 
     const loginHandler = (e) => {
         e.preventDefault();
@@ -14,16 +23,26 @@ const LoginForm = props => {
 
     const emailChangeHandler = (e) => {
         setEmailInput(e.target.value);
-        setLoginBtn(e.target.value.includes("@") &&
-                        e.target.value.includes(".") &&
-                        passwordInput.trim().length > 6);
+        // Use effect removed the need for this it seems
+        // setFormIsValid(e.target.value.includes("@") &&
+        //                 e.target.value.includes(".") &&
+        //                 passwordInput.trim().length > 6);
+    }
+
+    const validateEmailHandler = () => {
+        setEmailValid(emailInput.includes("@") && emailInput.includes("."));
     }
 
     const passwordChangeHandler = (e) => {
         setPasswordInput(e.target.value);
-        setLoginBtn(emailInput.includes("@") &&
-            emailInput.includes(".") &&
-            e.target.value.trim().length > 6);
+        // Use effect removed the need for this it seems
+        // setFormIsValid(emailInput.includes("@") &&
+        //     emailInput.includes(".") &&
+        //     e.target.value.trim().length > 6);
+    }
+
+    const validatePasswordHandler = () => {
+        setPasswordValid(passwordInput.trim().length > 6);
     }
 
 
@@ -31,19 +50,19 @@ const LoginForm = props => {
         <div className="row container login-form">
             <form className="col s12" onSubmit={loginHandler}>
                 <div className="row">
-                    <div className="input-field col s12">
-                        <input value={emailInput} onChange={emailChangeHandler} id="email" type="email" className="validate" />
+                    <div className={`${emailValid ? "input-field col s12": "error input-field col s12"}`}>
+                        <input onBlur={validateEmailHandler} value={emailInput} onChange={emailChangeHandler} id="email" type="email" className="validate" />
                             <label htmlFor="email">Email</label>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="input-field col s12">
-                        <input value={passwordInput} onChange={passwordChangeHandler} id="password" type="password" className="validate" />
+                    <div className={`${passwordValid ? "input-field col s12" : "error input-field col s12"}`}>
+                        <input onBlur={validatePasswordHandler} value={passwordInput} onChange={passwordChangeHandler} id="password" type="password" className="validate" />
                             <label htmlFor="password">Password</label>
                     </div>
                 </div>
                 <button
-                    disabled = {!loginInBtn}
+                    disabled = {!formIsValid}
                     className="btn">Login</button>
             </form>
         </div>
